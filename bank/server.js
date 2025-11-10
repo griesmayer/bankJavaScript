a053f6d047f35bbc4c1fe70321381a588124595b
Thomas Griesmayer <griesmayer@spengergasse.at>
2025-11-10 18:16:52 +0100

    get accounts implemented

--- bank/server.js
+++ bank/server.js
@@ -1,17 +1,28 @@
-// server.js
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// Import the built-in 'http' module
-const http = require('http');
+const customers = [
+  { id:  1, name: "Max",     balance: 100 },
+  { id:  2, name: "Susi",    balance: 300 },
+  { id:  3, name: "Fritz",   balance: 500 },
+  { id:  4, name: "Andrea",  balance: 150 },
+  { id:  5, name: "Thomas",  balance: 200 },
+  { id:  6, name: "Verena",  balance: 400 },
+  { id:  7, name: "Marion",  balance: 500 },
+  { id:  8, name: "Karl",    balance: 700 },
+  { id:  9, name: "Hans",    balance: 800 },
+  { id: 10, name: "Barbara", balance: 900 }
+];
 
-// Create a simple HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-   // Send the response text
-  res.end('Hello World\n');
+app.get("/", (req, res) => {
+  res.send("Hello, Thomas and World from Express!");
 });
 
-// The server will listen on port 3000
-server.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+app.get("/customers", (req, res) => {
+  res.json(customers);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
